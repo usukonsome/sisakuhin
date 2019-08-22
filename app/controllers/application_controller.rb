2d0fd797caf6caf544f57d23fe2cf4298cf7620f
usukonsome <49252753+usukonsome@users.noreply.github.com>
2019-08-22 12:16:13 +0900

    current

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  include PostsHelper
 
   def current_user
     session[:user] ||= request.remote_ip
--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,5 @@
 module PostsHelper
+  def current_user?(post)
+    post.user_id == @current_user.id
+  end
 end
