48cdfb32dbdc072363313a973156da4d48d4dbbf
usukonsome <49252753+usukonsome@users.noreply.github.com>
2019-09-03 10:21:39 +0900

    'byebye_remember_digest'

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,18 +4,17 @@ class User < ApplicationRecord
 
   def User.digest(string)
     BCrypt::Password.create(string)#ここをfooに変えてみる
-  end
+  end#仕様変更で今は使わない
 
   def User.new_token
-    SecureRandom.urlsafe_base64
+    SecureRandom.urlsafe_base64(14)
   end
 
   def remember
     self.digest = User.new_token
-    update_attribute(:remember_digest,User.digest(digest))
   end
 
   def authenticated?(string)
     BCrypt::Password.new(remember_digest).is_password?(string)
-  end
+  end#仕様変更で今は使わないその2
 end
