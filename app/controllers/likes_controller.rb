5327607e8c286ee21b68e73ab35979780efd71f2
usukonsome <49252753+usukonsome@users.noreply.github.com>
2019-09-03 16:29:13 +0900

    'tired'

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,7 +3,12 @@ class LikesController < ApplicationController
 
   def create
     @post = Post.find(params[:id])
-    Like.create(post_id: @post.id,user_digest: @current_user.digest)
+    like = Like.find_by(user_digest: @current_user.digest, post_id: @post.id)
+    if like == nil
+      Like.create(post_id: @post.id,user_digest: @current_user.digest)
+    else
+      既にいいねしてあるよ
+    end
   end
 
   def destroy
